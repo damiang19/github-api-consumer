fefab52886ed4c665c06c011a784157e4400885f
Damian <damiang19@gmail.com>
2023-09-06 22:36:26 +0200

    Add integration test for throwing exceptions

--- src/test/java/pl/damiang19/github/GithubApplicationTests.java
+++ src/test/java/pl/damiang19/github/GithubApplicationTests.java
@@ -2,6 +2,8 @@ package pl.damiang19.github;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class GithubApplicationTests {
